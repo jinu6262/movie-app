872bacadf2c67acd4e5fdabf2c7ee3578345b0f3
Kim JinWoo <kimjinu2540@gmail.com>
2023-05-18 16:50:42 +0900

    3. Headline 컴포넌트 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,11 @@
 import { Component } from "../core/core";
+import Headline from "../components/Headline";
 
 export default class Home extends Component {
     render() {
-        this.el.innerHTML = "<h1>Hi</h1>";
+        const headline = new Headline().el;
+
+        this.el.classList.add("container");
+        this.el.append(headline);
     }
 }
