cd63cb45bd8740d5fea37660312e1eb4401d31a0
Kim JinWoo <kimjinu2540@gmail.com>
2023-05-23 18:27:09 +0900

    9. 로딩 애니메이션 추

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -11,11 +11,16 @@ export default class MovieList extends Component {
         // MovieList 라는 컴포넌트가 만들어질때
         // 스토어의 movies 키 값이 변경되면 movies 키 이름으로 등록된 함수들을 실행
         // 지금은 옵저버객체에 movies 키에는 this.render(); 하나만 들어있음
+
+        movieStore.subscribe("loading", () => {
+            this.render();
+        });
     }
     render() {
         this.el.classList.add("movie-list");
         this.el.innerHTML = /*html*/ `
             <div class="movies"></div>
+            <div class="the-loader hide"></div>
         `;
 
         const moviesEl = this.el.querySelector(".movies");
@@ -24,5 +29,8 @@ export default class MovieList extends Component {
                 return new MovieItem({ movie: movie }).el;
             })
         );
+
+        const loaderEl = this.el.querySelector(".the-loader");
+        movieStore.state.loading ? loaderEl.classList.remove("hide") : loaderEl.classList.add("hide");
     }
 }
