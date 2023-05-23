b39d89faf4f718b7149cc993e9da02731fbcb19c
Kim JinWoo <kimjinu2540@gmail.com>
2023-05-23 19:08:19 +0900

    10. 예외 처리와 메시지 출력

--- src/components/MovieListMore.js
+++ src/components/MovieListMore.js
@@ -10,12 +10,18 @@ export default class MovieListMore extends Component {
             const { page, pageMax } = movieStore.state;
             pageMax > page ? this.el.classList.remove("hide") : this.el.classList.add("hide");
         });
+
+        // more 버튼이 있는 상태로 새로운 검색을 할때 more 버튼 지워주기
+        movieStore.subscribe("searchText", () => {
+            this.el.classList.add("hide");
+        });
     }
     render() {
         this.el.classList.add("btn", "view-more", "hide");
         this.el.textContent = "View more..";
 
         this.el.addEventListener("click", async () => {
+            this.el.classList.add("hide");
             await searchMovies(movieStore.state.page + 1);
         });
     }
